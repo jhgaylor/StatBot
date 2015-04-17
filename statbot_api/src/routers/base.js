549d0737cfb9630cf93472afd1da566b97e3413a
Jake Gaylor <jhgaylor@gmail.com>
2015-04-17 18:43:25 -0500

    status response

--- statbot_api/src/routers/base.js
+++ statbot_api/src/routers/base.js
@@ -1,6 +1,9 @@
 var BaseRouter = require('express').Router();
 
-BaseRouter.route('/');
+BaseRouter.route('/')
+  .get(function (err, res) {
+  	res.status(200).json({status: "success"});
+  });
 BaseRouter.route('/help');
 
 module.exports = BaseRouter;
