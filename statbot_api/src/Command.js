8cacaea0c98e7f81badd1d34f0ebaa8ce7150739
Jake Gaylor <jhgaylor@gmail.com>
2015-04-19 03:28:30 -0500

    trivial: remove cruft

--- statbot_api/src/Command.js
+++ statbot_api/src/Command.js
@@ -1,12 +1,5 @@
-var Q = require('q');
-var request = require('request');
 var _ = require('underscore');
-
 var DataSources = require('./DataSources');
-// use redis + expire
-// args needs to be kwargs (opts) so that it can be passed
-//   all over the place and every body know how to use the object
-// need to cache the raw data from responses (json/html) or the data structure i want used or both?
 
 var Command = function (data_sources, buildFn) {
   // returns a hashtable of data_source keys w/ a promise for its data
@@ -37,6 +30,8 @@ var Command = function (data_sources, buildFn) {
   }
 
   return {
+    // opts needs to be kwargs so that it can be passed
+    //   all over the place and every body know how to use the object
     run: function (opts) {
       var source_promise_getters = getDataSourceGetters()
       return buildFn(opts, source_promise_getters)
