048faaeb3fea0e54c90131d7ef1d37c7103353bb
Jake Gaylor <jhgaylor@gmail.com>
2015-04-20 04:08:53 -0500

    added route for id to name conversion

--- statbot_api/src/routers/base.js
+++ statbot_api/src/routers/base.js
@@ -1,9 +1,29 @@
 var BaseRouter = require('express').Router();
+var DataSources = require('../DataSources');
+var _ = require('underscore')
+var Q = require('q');
 
 BaseRouter.route('/')
   .get(function (err, res) {
   	res.status(200).json({status: "success"});
   });
-BaseRouter.route('/help');
+BaseRouter.route('/getSummonerNameById')
+  .get(function (req, res) {
+    var summoner_id = req.query.summoner_id;
+    console.log("looking up", summoner_id)
+    DataSources.riot.summoner_name.get({summoner_id: summoner_id})
+      .then(function (name) {
+        console.log("sending", name)
+        res.send({
+          data: {
+            summoner_id: summoner_id,
+            summoner_name: name
+          }
+        });
+      })
+      .catch(function (err) {
+        res.send({ error: res });
+      })
+  });
 
 module.exports = BaseRouter;
