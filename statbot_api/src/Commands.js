f5442a3460846a2a8714bb3dd0e684eb7bbe92c3
Jake Gaylor <jhgaylor@gmail.com>
2015-04-20 02:45:13 -0500

    update commands' async flow

--- statbot_api/src/Commands.js
+++ statbot_api/src/Commands.js
@@ -19,32 +19,54 @@ Commands = {
     });
     return results_promise;
   }),
-  win_loss: Command(['riot.ranked_stats'], function (opts, dataSourceGetters) {
-    var stats_promise = dataSourceGetters['riot.ranked_stats'](opts)
-    var results_promise = stats_promise.then(function (stats) {
-      var champions = [];
-      var wins = 0;
-      var losses = 0;
-      champions = stats.champions;
-      _.each(champions, function (champion) {
-        // TODO: take into consideration champion_name if set
-        _.each(champion.stats, function (val, key, obj) {
-          if (key === "totalSessionsWon") {
-            wins += val;
-          }
+  win_loss: Command(['riot.ranked_stats', 'riot.static_champions_data'], function (opts, dataSourceGetters) {
+    console.log(opts)
+    var stats_promise = dataSourceGetters['riot.ranked_stats'](opts);
+    var champions_data_promise = dataSourceGetters['riot.static_champions_data'](opts);
+    // wait for all data sources and call a handler to
+    var results_promise = Q.all([stats_promise, champions_data_promise])
+      .spread(function (statsPerChampion, lookups){
+        // console.log("command received", statsPerChampion, lookups);
+        var champions = [];
+        var wins = 0;
+        var losses = 0;
 
-          if (key === "totalSessionsLost") {
-            losses += val;
+        if(opts.champion_name) {
+          // only calculate the stats for the champion in question
+          championId = lookups.lookupByName[opts.champion_name];
+          if (! championId) {
+            return {error: "Invalid champion name provided"};
           }
-        });
+          // find the champion matching the id from the stats
+          var champ = _.find(statsPerChampion, function (el) {
+            return el.id == championId
+          });
+          if (champ) {
+            wins = champ.stats.totalSessionsWon
+            losses = champ.stats.totalSessionsLost
+          }
+        } else {
+          // calculate stats for all champions
+          _.each(statsPerChampion, function (champion, championId) {
+            // TODO: take into consideration champion_name if set
+            _.each(champion.stats, function (val, key, obj) {
+              if (key === "totalSessionsWon") {
+                wins += val;
+              }
+
+              if (key === "totalSessionsLost") {
+                losses += val;
+              }
+            });
+          });
+        }
+
+        return {
+          wins: wins,
+          losses: losses,
+          winRate: ((wins/(wins+losses))*100).toFixed(2)
+        };
       });
-
-      return {
-        wins: wins,
-        losses: losses,
-        winRate: ((wins/(wins+losses))*100).toFixed(2)
-      }
-    });
     return results_promise;
   }),
 }
