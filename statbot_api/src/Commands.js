fefdd2dd6d263ade22ec50a30364c40fabea8d55
Jake Gaylor <jhgaylor@gmail.com>
2015-04-20 06:01:35 -0500

    add counters command
    this is the first example of scraping HTML for data

--- statbot_api/src/Commands.js
+++ statbot_api/src/Commands.js
@@ -2,10 +2,35 @@ var Command = require('./Command');
 
 var _ = require('underscore')
 var Q = require('q');
+
+// serverside implementation of the jQuery api
+var cheerio = require('cheerio');
 // NOTE: For each data_source listed, there will be a key accessible in
 //   the builderFn.  Only list a data_source if builder depends directly on the data
 //   Do NOT list subdeps
 Commands = {
+  counters: Command(['championselect.champion'], function (opts, dataSourceGetters) {
+    var results_promise = dataSourceGetters['championselect.champion'](opts)
+    results_promise = results_promise.then(function (html) {
+      // extract the data from the html
+      var $ = cheerio.load(html);
+      // TODO: extend to work for specified lanes by replacing _general
+      var $champs = $('._general .weak-block .champ-block .left.theinfo');
+      var counters = [];
+      $champs.each(function (i, el) {
+        $el = $(el);
+        var counter = {
+          name: $el.find('a.left').text(),
+          lane: $el.find('.info .lane').text(),
+          upvotes: $el.find('.info .uvote').text(),
+          downvotes: $el.find('.info .dvote').text()
+        };
+        counters.push(counter);
+      });
+      return counters;
+    });
+    return results_promise;
+  }),
   free: Command(['riot.free_champions'], function (opts, dataSourceGetters) {
     // build the data structure that should be send from the API to the caller
     // now have access to the data source values by calling getDataSourceGetters(options) which will return a promise for the data in that source
